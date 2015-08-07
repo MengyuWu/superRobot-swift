df3edb6f681250a8bdd0a3177f76b9ff8b35993c
Mengyu Wu <mw2907@columbia.edu>
2015-08-07 11:05:35 -0400

    add webView

--- superRobotSwift/viewController/ConnectViewController.swift
+++ superRobotSwift/viewController/ConnectViewController.swift
@@ -21,6 +21,7 @@ class ConnectViewController: UIViewController, NSStreamDelegate  {
     @IBOutlet weak var IPTextField: UITextField!
     @IBOutlet weak var PortTextField: UITextField!
     
+    @IBOutlet weak var webView: UIWebView!
  
 
     @IBAction func ConnectButtonPressed(sender: AnyObject) {
@@ -62,23 +63,22 @@ class ConnectViewController: UIViewController, NSStreamDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+     
+        var urlAddress="https://www.youtube.com"
+        var url=NSURL(string: urlAddress)
+        let request = NSURLRequest(URL: url!)
+        webView.loadRequest(request)
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
+    
     }
-    */
-
 }
